7767c2cbc7de8d4aa57381c7e6759cc073918212
Sararattai <39612325+Sararattai@users.noreply.github.com>
2018-05-31 15:18:44 +0530

    Create by using raw input.py

---
+++ by using raw input.py
@@ -0,0 +1,7 @@
+raw_data = {'first_name': ['Jason', 'Molly', 'Tina'], 
+        'last_name': ['Miller','Milner', 'Cooze'], 
+        'address' : ['Jacaranda','Robo St','Spencer Dr']
+        'age': [42, 52, 36], 
+        'city' : ['San' ,'JoseFremont','Stockton']
+f = (raw_data, columns = ['first_name', 'last_name', 'age', 'city'])
+print(f)
