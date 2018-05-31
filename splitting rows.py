e3920ad2a6924a332dfd0214cc22e000fae57424
Sararattai <39612325+Sararattai@users.noreply.github.com>
2018-05-31 11:25:53 +0530

    Create splitting rows.py

---
+++ splitting rows.py
@@ -0,0 +1,11 @@
+import sys
+user_name = []
+f = open('numbers.csv')
+data = read.f
+row = data.split('\n')
+for row in rows:
+   split_row= row.split(',')
+   user_name.append(split_row)
+   print(user_name)
+   
+  
