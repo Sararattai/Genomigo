d6104ed72c04cb3510934e890a9b212f3384e82c
Sararattai <39612325+Sararattai@users.noreply.github.com>
2018-06-04 21:18:52 +0530

    edits

---
+++ editsscratch2.py
@@ -0,0 +1,12 @@
+import sys
+with open('number.csv','r') as csv_file:
+    csv_reader = red(csv_file)
+    age_list = []
+for row in csv_reader:
+    row.appendall(row[3].differencs row[4])
+    age_list.append(row)  #appending age in row 5
+    writer[csv_file]=write.row[age_list]
+    print["age_list"]
+
+
+
