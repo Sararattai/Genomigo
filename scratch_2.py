63680d0f0bfa6cf05886634fb863df99ecf91093
Sararattai <39612325+Sararattai@users.noreply.github.com>
2018-06-04 10:42:51 +0530

    Update scratch_2.py

--- scratch_2.py
+++ scratch_2.py
@@ -5,8 +5,8 @@ with open('number.csv','r') as csv_file:
 for row in csv_reader:
     row.appendall(row[3]-row[4])
     age_list.append(row)  #appendig age in row 5
-writer[csv_file]=write.row[age.list]
-print["age_list"]
+    writer[csv_file]=write.row[age.list]
+    print["age_list"]
 
 
 
